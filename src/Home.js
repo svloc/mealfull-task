813e3a05b59e681c82f26cf27663891973c9564f
svloc <svloc39@gmail.com>
2022-04-13 19:25:06 +0530

    added filter for date slot

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,15 @@ ChartJS.register(
 
 export default function Home(p) {
   let { date } = p;
-  const [showMore,setShowMore]=useState(false);
+  const [showMore, setShowMore] = useState(false);
+  const [moreData, setMoreData] = useState([]);
+  
+
+  useEffect(() => {
+    if (showMore) {
+      get_more_info();
+    }
+  });
   const get_data = () => {
     const filter_date = arrData.filter((a) => {
       const split_date = a.item_date.split("-");
@@ -53,57 +61,76 @@ export default function Home(p) {
   };
 
   const datas = get_data();
-  console.log(datas);
+  
 
-  const get_more_info = (a) => {
-    console.log(a);
-  };
+  const get_more_info = () => {
+    let dateArr=[];
+    let hours = [
+      {hoursName: '9am to 12pm', from: 9, to: 12},
+      {hoursName: '12pm to 3pm', from: 12, to: 15},
+      {hoursName: '3pm to 6pm', from: 15, to: 18},
+      {hoursName: '6pm to 9pm', from: 18, to: 21}];
+   
+   const targetDate = new Date("2021-05-20");
+   
+   const filter_by_date= arrData.filter((e)=>{
+      return e.schedule_time.split(" ")[0].substring(5,10)==moreData.split('-')[1]+'-'+moreData.split('-')[0];
+    })
+
+    // filter_by_date.filter((f)=>{
+    //   dateArr.push(new Date('1970-01-01T' + f.schedule_time.split(' ')[1] + 'Z').toLocaleTimeString('en-US',
+    //   {timeZone:'UTC',hour12:true,hour:'numeric',minute:'numeric'}));});
+    
+    const previousItems = arrData.filter(({ schedule_time }) => {
+      const scheduleTime = new Date(schedule_time);
+      return scheduleTime < targetDate;
+    });
+    const result = previousItems.reduce((acc, { schedule_time }) => {
+      const scheduleTime = new Date(schedule_time);
+      const targetHour = scheduleTime.getHours();
+      const { hoursName } = hours.find(({ from, to }) => (targetHour >= from) && (targetHour < to));
+      acc[hoursName] = (acc[hoursName] ?? 0) + 1;
+      return acc;
+    }, {});
+    console.log(result);
+ };
 
   return (
     <div className="graph-component">
-      {!showMore && <Bar 
-      //  onClick={elem => {
-      //    console.log(elem)
-      //   // var data = datas[elem[0]._datasetIndex];
-      //   // console.log("Cases", data[elem[0]._index]);
-    
-      //   // data = datas[elem[1]._datasetIndex];
-      //   // console.log("Recovered", data[elem[1]._index]);
-      // }}
-        data={{
-          labels: datas[0],
-          datasets: [
-            {
-              label: "Schedule Count",
-              data: datas[1],
-              backgroundColor: "#342df2a6",
+      {!showMore && (
+        <Bar
+          data={{
+            labels: datas[0],
+            datasets: [
+              {
+                label: "Schedule Count",
+                data: datas[1],
+                backgroundColor: "#342df2a6",
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            plugins: {
+              title: {
+                display: true,
+                text: "Scheduled Per Day",
+              },
             },
-          ],
-        }}
-        options={{
-          responsive: true,
-          plugins: {
-            title: {
-              display: true,
-              text: "Scheduled Per Day",
-            },
-          },
-          onClick: function(evt, element) {
-            if(element.length > 0) {
-                const obj={
-                  date:datas[0][element[0].index],
-                  quntity:datas[1][element[0].index]
-                }
+            onClick: function (evt, element) {
+              if (element.length > 0) {
+                setMoreData(datas[0][element[0].index]);
                 setShowMore(true);
-            }}
-        }}
-      />}
-      {showMore &&
+              }
+            },
+          }}
+        />
+      )}
+      {showMore && (
         <div>
-        <p>More Info part</p>
-        <button onClick={()=> setShowMore(false)}>Back</button>
+          <button onClick={() => setShowMore(false)}>Back</button>
         </div>
-      }
+      )}
     </div>
   );
 }
